7b0423971e235a3b9346874bd768c4ffd3f4e55f
Niels Drost <nielsdrost7@gmail.com>
2026-01-03 10:37:42 +0100

    Just a debugging page, to see if TailwindCss works correctly

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -4,6 +4,9 @@ namespace App\Filament\Resources\Users\Schemas;
 
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Fieldset;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class UserForm
@@ -12,16 +15,29 @@ class UserForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->required(),
-                TextInput::make('email')
-                    ->label('Email address')
-                    ->email()
-                    ->required(),
-                DateTimePicker::make('email_verified_at'),
-                TextInput::make('password')
-                    ->password()
-                    ->required(),
+                Grid::make(4)
+                    ->columnSpanFull()
+                    ->schema([
+                        Section::make('Personal Information')
+                            ->columnSpan(2)
+                            ->columns(1)
+                            ->schema([
+                                TextInput::make('name')
+                                    ->required(),
+                                TextInput::make('email')
+                                    ->label('Email address')
+                                    ->email()
+                                    ->required(),
+                            ]),
+                        Fieldset::make('Account')
+                            ->columnSpan(2)
+                            ->schema([
+                                DateTimePicker::make('email_verified_at'),
+                                TextInput::make('password')
+                                    ->password()
+                                    ->required(),
+                            ]),
+                    ]),
             ]);
     }
 }
--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Users\Tables;
 
+use Filament\Actions\ActionGroup;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
@@ -34,7 +35,9 @@ class UsersTable
             ->filters([
             ])
             ->recordActions([
-                EditAction::make(),
+                ActionGroup::make([
+                    EditAction::make(),
+                ]),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
