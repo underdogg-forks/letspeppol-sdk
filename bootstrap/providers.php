69740fedba6df7e90fee7adcdcb078608ebe1c1d
Niels Drost <nielsdrost7@gmail.com>
2026-01-03 07:20:50 +0100

    Initial commits

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\CompanyPanelProvider::class,
+    App\Providers\Filament\UserPanelProvider::class,
 ];
